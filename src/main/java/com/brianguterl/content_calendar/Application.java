f33cbdbb296ef9957fdf417170de699f00d10588
Brian Guterl <brianguterl99@gmail.com>
2024-10-10 22:26:48 -0400

    written examples

--- src/main/java/com/brianguterl/content_calendar/Application.java
+++ src/main/java/com/brianguterl/content_calendar/Application.java
@@ -2,12 +2,19 @@ package com.brianguterl.content_calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		RestTemplate restTemplate = (RestTemplate) context.getBean("restTemplate");
+		System.out.println(restTemplate);
+
 	}
 
 }
